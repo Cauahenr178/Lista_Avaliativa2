ef025be413ac1b9eedc756220d43f035d6b46b79
Cauahenr178 <cauahenrique690@gmail.com>
2023-11-07 12:30:10 -0300

    Operacoes

--- Questao3.c
+++ Questao3.c
@@ -44,7 +44,7 @@ int main(){
     for (int i = 0; i < 4; i++){
        for (int j = 0; j < 4; j++){
             printf("%4d", resultado[i][j]);
-       }
+      }
         printf("\n");
    }
     
