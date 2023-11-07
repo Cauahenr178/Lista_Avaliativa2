17fb7fa19c43c42549e7a61fcde01d7d09d531c0
Cauahenr178 <cauahenrique690@gmail.com>
2023-11-07 20:16:08 -0300

    upgrade Operacao

--- Questao3.c
+++ Questao3.c
@@ -2,19 +2,19 @@
 #include <string.h>
 
 int main(){
-   int a[4][4];
-   int b[4][4];
+   int Matriz_A[4][4];
+   int Matriz_B[4][4];
    int resultado[4][4];
    char operacao[3];
     
     for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-         scanf("%d", &a[i][j]);
+      for (int j = 0; j < 4; j++){
+         scanf("%d", &Matriz_A[i][j]);
         }
     }
     for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-          scanf("%d", &b[i][j]);
+      for (int j = 0; j < 4; j++){
+          scanf("%d", &Matriz_B[i][j]);
        }
     }
      scanf("%s", operacao);
@@ -22,21 +22,21 @@ int main(){
     if (strcmp(operacao, "soma") == 0){
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++){
-              resultado[i][j] = a[i][j] + b[i][j];
+              resultado[i][j] = Matriz_A[i][j] + Matriz_B[i][j];
           }
         }
     } else if(strcmp(operacao, "sub") == 0){
         for (int i = 0; i < 4; i++){
             for (int j = 0; j < 4; j++){
-            resultado[i][j] = a[i][j] - b[i][j];
-            }
+            resultado[i][j] = Matriz_A[i][j] - Matriz_B[i][j];
+          }
         }
     } else if(strcmp(operacao, "mult") == 0){
         for (int i = 0; i < 4; i++){
            for (int j = 0; j < 4; j++){
                 resultado[i][j] = 0;
                 for (int k = 0; k < 4; k++){
-                 resultado[i][j] += a[i][k] * b[k][j];
+                 resultado[i][j] += Matriz_A[i][k] * Matriz_B[k][j];
               }
           }
         }
@@ -45,8 +45,7 @@ int main(){
        for (int j = 0; j < 4; j++){
             printf("%4d", resultado[i][j]);
       }
-        printf("\n");
+       printf("\n");
    }
-    
     return 0;
 }
